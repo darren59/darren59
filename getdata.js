46cf887ec7aa10173d9a1ab2091b47d9a4d1b2e7
Darren Ng <darren_59@yahoo.com>
2022-02-18 13:25:22 +0800

    add html and js files

---
+++ getdata.js
@@ -0,0 +1,11 @@
+//syntax: $.getJSON(url, [data], [callback]);
+//AJAX library
+// const b1 = document.getElementById("b1");
+// b1.addEventListener("click", getFromServer);
+
+// $(".mypanel").html("Hello there!")
+
+let names = ["Dixant", "Jon", "Keith"];
+
+$(".mypanel").html("Hello, "+ names[])
+
